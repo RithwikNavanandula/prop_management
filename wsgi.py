1bd302aa6c8b339beab562abebed463ab2f47e2c
RithwikNavanandula <rithwiknavanandula@gmail.com>
2026-02-17 15:09:15 +0530

    k

--- wsgi.py
+++ wsgi.py
@@ -2,14 +2,15 @@ import os
 import sys
 
 # Add the project directory to the python path
-project_home = os.path.expanduser('~/Property Management V2')
+# Add the project directory to the python path
+project_home = os.path.expanduser('~/prop_management')
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
 # Set environment variables for SQLite with an absolute path
 # Replace 'yourusername' with your actual PythonAnywhere username if different
 username = os.environ.get('USER', 'yourusername')
-os.environ['DATABASE_URL'] = f'sqlite:////home/{username}/Property Management V2/property_mgmt.db'
+os.environ['DATABASE_URL'] = f'sqlite:////home/{username}/prop_management/property_mgmt.db'
 
 from app.main import app
 from asgiref.wsgi import WsgiToAsgi
